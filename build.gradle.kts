db6739ff25c13837027d2e636ef75df66efaa7c0
Dayan Ruben <mail@dayanruben.com>
2025-04-16 14:19:43 -0300

    Update copyright years to 2025

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>().configureEach 
         mapOf(
             "org.jetbrains.dokka.base.DokkaBase" to """
             {
-                "footerMessage": "Copyright &copy; 2022 Derek Ellis",
+                "footerMessage": "Copyright &copy; 2025 Derek Ellis",
                 "customStyleSheets": ["${file("docs/css/logo-styles.css").invariantSeparatorsPath}"]
             }
         """.trimIndent()
